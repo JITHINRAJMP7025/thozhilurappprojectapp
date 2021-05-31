8b8b3d2f756e5c5ec5738f309d69734ab24aba1a
JITHINRAJMP7025 <jithinrajmp@cet.ac.in>
2021-05-30 21:16:25 -0700

    update

---
+++ app/src/main/java/com/example/thozhilurapplastfinal/workdetails.java
@@ -0,0 +1,14 @@
+package com.example.thozhilurapplastfinal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class workdetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workdetails);
+    }
+}
